6c81bc9ec0ef6f50c5123d94394f3794d86ea67f
ShunyaSuzuki <anyushu2017@gmail.com>
2020-03-27 10:47:08 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import MyPage from './Components/MyPage';
 import Cart from './Components/Cart';
 import './sass/app.scss';
 
 function App() {
-  return (<BrowserRouter>
-    <Switch>
-      <Route exact path="/" children={<Home />}/>
-      <Route path="/mypage" children={<MyPage />}/>
-      <Route path="/cart" children={<Cart />}/>
-    </Switch>
-  </BrowserRouter>);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" children={<Home />} />
+        <Route path="/mypage" children={<MyPage />} />
+        <Route path="/cart" children={<Cart />} />
+      </Switch>ı
+    </BrowserRouter>
+  );
 }
 
 export default App;
