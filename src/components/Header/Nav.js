01fa8ed7abfda6bf1ee88d748ec6b8275e7d5511
ShunyaSuzuki <anyushu2017@gmail.com>
2020-03-27 11:05:49 +0900

    update

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import logo from '../../img/logo.svg';
 
-import {Layout, Row, Col} from 'antd';
-import {Briefcase, User} from 'react-feather';
+import { Layout, Row, Col } from 'antd';
+import { Briefcase, User } from 'react-feather';
 
-const {Header} = Layout;
+const { Header } = Layout;
 
 export default class Nav extends React.Component {
   render() {
-    return (<Header>
-      <Row justify="space-between">
-        <Col>
-          <div className="logo">
-            <img src={logo} alt="logo"/>
-          </div>
-        </Col>
-        <Col>
-          <Row>
-            <Col>
-              <Link to="/cart">
-                <Briefcase size={20}/>
-                <span>カート</span>
-              </Link>
-            </Col>
-            <Col>
-              <Link to='/mypage'>
-                <User size={20}/>
-                <span>マイページ</span>
-              </Link>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Header>);
+    return (
+      <Header className="pc-header">
+        <Row justify="space-between">
+          <Col>
+            <div className="logo">
+              <img src={logo} alt="logo" />
+            </div>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <Link to="/cart">
+                  <Briefcase size={20} />
+                  <span>カート</span>
+                </Link>
+              </Col>
+              <Col>
+                <Link to='/mypage'>
+                  <User size={20} />
+                  <span>マイページ</span>
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Header>
+    );
   }
 }
